4b7aee895675a7da640afb4d0b116d9c199b906e
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-05-09 15:48:05 -0300

    Alteração na baseURL da camada de serviço novamente.

--- src/app/evento/evento.service.ts
+++ src/app/evento/evento.service.ts
@@ -4,8 +4,8 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class EventoService {
 
-  //eventosUrl = 'http://localhost:8080/eventos';
-  eventosUrl = 'https://postgres://nhcafttttpbeku:8de8224ae193af258288f3e5589f8fd84ba03f275b7b01741bc96981839ec7c2@ec2-54-204-39-46.compute-1.amazonaws.com:5432/ddu2o8v202apch/eventos';
+  //eventosUrl = 'http://localhost:8080/eventos';  
+  eventosUrl = 'ec2-54-204-39-46.compute-1.amazonaws.com/eventos';
 
   constructor(private http: HttpClient) { }
 
