57a66de2ba3ff6b3e5eac951dbd64d12f3afc7d0
Rafael Calearo <rafaelcalearo@hotmail.com>
2018-05-19 21:10:01 -0300

    Alterando tela de login e adicionando o Primeng.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+//DISPONIBILIZA O RECURSO DE CALENDARIO NO INPUT DO TIPO "p-calendar" DO PRIMENG
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//DISPONIBILIZA RECURSO DE FORMULARIO DO ANGULAR
+import { FormsModule } from '@angular/forms';
+//IMPORTACAO USADA P/ A CRIACAO DE ROTAS
+import { RouterModule } from '@angular/router';
+//IMPORTACOES USADAS P/ A VALIDACAO DO FORM S/ O PRIMENG
+//import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//IMPORTACOES USADAS P/ O PRIMENG
+import { DataTableModule, ButtonModule, InputTextModule, InputMaskModule, CalendarModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { ROUTES } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 import { EventoService } from './evento/evento.service';
-import { RouterModule } from '@angular/router';
-import { ROUTES } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 
 
@@ -25,9 +34,17 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(ROUTES),
     FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(ROUTES)
+
+    //FormsModule,
+    //ReactiveFormsModule,
+    DataTableModule,
+    ButtonModule,
+    InputTextModule,
+    InputMaskModule,
+    CalendarModule    
   ],
   providers: [
     EventoService
